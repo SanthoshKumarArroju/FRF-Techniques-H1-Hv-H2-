805274ad45ed20b4e5719ea1d1dbde7fd360b68e
Santhosh Kumar Arroju <31964108+SanthoshKumarArroju@users.noreply.github.com>
2018-08-10 18:56:48 -0400

    minor changes

--- Lab3_hammer.m
+++ Lab3_hammer.m
@@ -5,28 +5,28 @@ clear all
 clc
 load('test_3.mat')
 
-f = Freq_domain;
-Sfx = Hf_Cross_Spec_chan_2; 
-Sxf = conj(Sfx);                   
-Sxx = PSD_chan_2;           
-Sff = PSD_chan_1;        
-H1 = (Sfx./Sff).';                          % H1 estimate
-H2 = (Sxx./Sxf).';                          % H2 estimate
-Hv = ((Sfx./abs(Sfx)).*(sqrt(Sxx./Sff)))';  % Hv estimate
+F = Freq_domain;
+SFX = Hf_Cross_Spec_chan_2; 
+SXF = conj(SFX);                   
+SXX = PSD_chan_2;           
+SFF = PSD_chan_1;        
+H1 = (SFX./SFF).';                          % H1 estimate
+H2 = (SXX./SXF).';                          % H2 estimate
+Hv = ((SFX./abs(SFX)).*(sqrt(SXX./SFF)))';  % Hv estimate
 
 figure(1)
-plot(f,20*log10(abs(H1)),'b')
+plot(F,20*log10(abs(H1)),'b')
 hold on
-plot(f,20*log10(abs(Hv)),'r')
+plot(F,20*log10(abs(Hv)),'r')
 hold on
-plot(f,20*log10(abs(H2)),'k')
+plot(F,20*log10(abs(H2)),'k')
 title('FRF of impact hammer test')
 legend('H1','Hv','H2')
 coh = H1/H2;   % 0.2123
-coherence = abs(coh)
+coherence = abs(coh);
 
-noise_op = (1-coh)*Sxx;   % noise in output signal estimation
-noise_ip = (1-coh)*Sff;  % noise in output signal estimation
+noise_op = (1-coh)*SXX;   % noise in output signal estimation
+noise_ip = (1-coh)*SFF;  % noise in output signal estimation
 
 figure(2)
 plot(Freq_domain,noise_ip)
